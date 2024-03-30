e12a4a0d643dc64432d835fd69968bcc89ea96e5
Kumassy <kumassyii@gmail.com>
2024-03-30 06:41:27 +0000

    add netlink call

--- src/bin/exporter.rs
+++ src/bin/exporter.rs
@@ -1,10 +1,68 @@
 
 use anyhow::{anyhow, bail, Context, Result};
-use std::net::SocketAddr;
+use getdelays_rs::{create_nl_socket, get_family_id, print_delayacct, print_ioacct, send_delay_request, task_context_switch_counts, taskstats::{Taskstats, TaskstatsCtrl, TaskstatsTypeAttrs}};
+use netlink_packet_core::{NetlinkHeader, NetlinkMessage, NetlinkPayload};
+use netlink_packet_generic::GenlMessage;
+use netlink_sys::Socket;
+use std::{mem::size_of, net::SocketAddr, sync::Arc};
 use prometheus_exporter::prometheus::{self, labels, register_gauge_vec};
+use log::*;
 
 fn main() -> Result<()> {
     pretty_env_logger::init();
+
+    let socket = create_nl_socket().context("error creating Netlink socket")?;
+    let socket = Arc::new(socket);
+    let family_id = get_family_id(&socket).context("Error getting family id")?;
+
+    if family_id == 0 {
+        bail!("Error getting family id");
+    }
+    debug!("family id {}", family_id);
+
+    let reader_socket = socket.clone();
+    std::thread::spawn( move || {
+        loop {
+            let mut rxbuf = vec![0; 4096];
+
+            let rep_len = reader_socket
+                .recv(&mut &mut rxbuf[..], 0).unwrap();
+            debug!("received {} bytes", rep_len);
+    
+            let response = <NetlinkMessage<GenlMessage<TaskstatsCtrl<TaskstatsTypeAttrs>>>>::deserialize(
+                &rxbuf[0..(rep_len as usize)],
+            )
+            .context("fatal reply error: unable to parse Netlink Packet").unwrap();
+            debug!(
+                "nlmsghdr size={}, nlmsg_len={}, rep_len={}",
+                size_of::<NetlinkHeader>(),
+                response.buffer_len(),
+                rep_len
+            );
+    
+            match response.payload {
+                NetlinkPayload::Error(err) => {
+                    // bail!("fatal reply error: {}", err);
+                }
+                NetlinkPayload::InnerMessage(genlmsg) => {
+                    for nla in genlmsg.payload.nlas.iter() {
+                        match nla {
+                            TaskstatsTypeAttrs::AggrPid(rtid, stats) | TaskstatsTypeAttrs::AggrTgid(rtid, stats) => {
+                                print_delayacct(stats);
+                                print_ioacct(stats);
+                                task_context_switch_counts(stats);
+                            }
+                            _ => {
+                                warn!("Unknown nla_type {:?}", nla);
+                            }
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+    });
+
 
     let addr: SocketAddr = "0.0.0.0:9186".parse()?;
 
@@ -12,8 +70,6 @@ fn main() -> Result<()> {
     let exporter = prometheus_exporter::start(addr)?;
 
     // list pids of all processes in the system
-
-
     loop {
         let _guard = exporter.wait_duration(std::time::Duration::from_secs(5));
 
@@ -27,6 +83,9 @@ fn main() -> Result<()> {
             let memory_usage = process.stat()?.rss * procfs::page_size();
 
             gauge_vec.with_label_values(&[&pid.to_string(), &process.stat()?.comm]).set(cpu_usage as f64);
+
+            debug!("send delay request for pid {}", pid);
+            send_delay_request(&socket, family_id, pid as u32)?;
         }
     }
 
